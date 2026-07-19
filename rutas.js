54f13c0398114132c6c75086cd531fe18c239815
J. Francisco <gutierrezfrancisco081@gmail.com>
2026-07-18 23:41:21 -0600

    se implemeta bd improvisada

---
+++ rutas.js
@@ -0,0 +1,50 @@
+// Archivo: rutas.js
+// Aquí solo guardamos la información pura (Base de datos simulada)
+
+const rutasPublicas = [
+    {
+        id: 'R10',
+        nombre: 'Ruta 10',
+        origen: 'C.U. BUAP',
+        destino: 'Centro Histórico',
+        color: '#0066CC',
+        costo: '$8.50 MXN',
+        frecuencia: '10 min',
+        coordenadas: [
+            [19.0016, -98.2003], [19.0125, -98.2045], 
+            [19.0300, -98.2010], [19.0430, -98.1980]
+        ]
+    },
+    {
+        id: 'M',
+        nombre: 'Ruta 11',
+        origen: 'San Antonio Mihuacan',
+        destino: 'Centro',
+        color: '#8e44ad',
+        costo: '$8.50 MXN',
+        frecuencia: '15 min',
+        coordenadas: [
+            [19.1529804, -98.3025707], [19.1539823, -98.3039922],
+            [19.1541741, -98.3042242], [19.1542448, -98.3042468],
+            [19.1543777, -98.3042609], [19.1544474, -98.3042774],
+            [19.1545132, -98.3043363], [19.1546993, -98.3045999],
+            [19.1552038, -98.3052747], [19.1552452, -98.3053039]
+        ]
+    },
+    {
+        id: 'apx',
+        nombre: 'Ruta Mihuacan',
+        origen: 'San Antonio Mihuacan',
+        destino: 'Centro',
+        color: '#8e44ad',
+        costo: '$8.50 MXN',
+        frecuencia: '15 min',
+        coordenadas: [
+            [19.1529804, -98.3025707], [19.1539823, -98.3039922],
+            [19.1541741, -98.3042242], [19.1542448, -98.3042468],
+            [19.1543777, -98.3042609], [19.1544474, -98.3042774],
+            [19.1545132, -98.3043363], [19.1546993, -98.3045999],
+            [19.1552038, -98.3052747], [19.1552452, -98.3053039]
+        ]
+    }
+];
